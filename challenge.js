1ed91abf09d8b1e1f978dd700fe57f7973bf017c
owenhuis <143698007+owenhuis@users.noreply.github.com>
2025-05-21 14:15:39 +0200

    js push

--- challenge.js
+++ challenge.js
@@ -1,2 +1,31 @@
-const staafdiagram = document.createElement("div")
+const grafiek = document.getElementsByClassName("grafiek")
 
+const staafdiagram = document.createElement("div");
+staafdiagram.id= "staafdiagram";
+staafdiagram.style.backgroundColor = "grey";
+staafdiagram.style.height = "300px";
+staafdiagram.style.width = "400px";
+
+// for loop in de recente informatie 
+// (voeg toe tot klaar dan maak gemiddelde doe dat over meerdere dagen kijk naar dagen tot dit punt en kijk waar het uit zo komen)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+grafiek.appendChild(staafdiagram);
+
+document.body.appendChild(grafiek);
